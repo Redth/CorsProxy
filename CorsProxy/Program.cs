831895b6b6233f183fe461fdddd0667922eb62da
redth <jondick@gmail.com>
2023-09-14 11:34:17 -0400

    Customize http invoker
    Allow any cert

--- CorsProxy/Program.cs
+++ CorsProxy/Program.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Net;
+using CorsProxy;
 using Yarp.ReverseProxy.Forwarder;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,12 +22,33 @@ app.UseCors(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 var config = app.Configuration.GetSection("CorsProxy");
 var destinationUrl = config.GetValue<string>("DestinationUrl")!;
 
+var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
+{
+    UseProxy = false,
+    AllowAutoRedirect = true,
+    AutomaticDecompression = DecompressionMethods.None,
+    UseCookies = false,
+    ActivityHeadersPropagator = new ReverseProxyPropagator(DistributedContextPropagator.Current),
+    ConnectTimeout = TimeSpan.FromSeconds(15),
+    SslOptions = new System.Net.Security.SslClientAuthenticationOptions
+    {
+        RemoteCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback((sender, cert, chain, err) =>
+        {
+            return true;
+        })
+    }
+}) ;
+
 app.MapForwarder(
     "{**catch-all}",
-    destinationUrl, 
+    destinationUrl,
     new ForwarderRequestConfig
     {
-        ActivityTimeout = TimeSpan.FromSeconds(100)
-    }, HttpTransformer.Default);
+        ActivityTimeout = TimeSpan.FromSeconds(100),
+        Version = new Version(1, 1)
+    },
+    HttpTransformer.Default);
+    //new CustomTransformer(app.Configuration),
+    //httpClient);
 
 app.Run();
